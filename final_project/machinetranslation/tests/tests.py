1b0a42c3280f044fa3bf7ba6f7eae4fd6f2ddf18
Riccardo <riccardodarcangelo@live.de>
2023-06-14 15:58:50 +0200

    initial

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,11 @@
+from deep_translator import MyMemoryTranslator
+
+def english_to_french(english_text):
+    french_text = MyMemoryTranslator(source="en", target="fr").translate(english_text)
+    print(french_text)
+    return french_text
+
+def french_to_english(french_text):
+    english_text = MyMemoryTranslator(source="fr", target="en").translate(french_text)
+    print(english_text)
+    return english_text
